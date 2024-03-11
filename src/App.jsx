7c92e31a38a854552298d9fe1dd99b63131a1104
Shameel Ahmad <shameel2050@gmail.com>
2024-03-11 13:09:40 +0500

    To-do-list

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Tasklist from "./Components/Tasklist/Tasklist"
+import Task from "./Components/Task/Task"
+const App = ()=>{
+  
+
+  return(
+    <div>
+      <Task/>
+      {/* <Tasklist/> */}
+     
+    </div>
+  )
+}
+
+export default App
